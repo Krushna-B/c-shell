582a55ebf63172b3e1ab924872f7985b7cc89f6b
Krushna-B <Krushna.Bhanushali@unc.edu>
2025-12-16 14:53:36 -0500

    Finished shell

--- src/main.c
+++ src/main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define LSH_BUFFER 1024
 #define LSH_TOKEN_BUFF 64
-#define LSH_DELIM "\t\r\n\a"
+#define LSH_DELIM " \t\r\n\a"
 
 void lsh_loop();
 char *lsh_readline();
 char **lsh_split_line(char *line);
-int lsh_execute_line();
+int lsh_execute_line(char **args);
+int lsh_num_builtins();
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+// Built in commands
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"};
+
+int (*builtin_func[])(char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit};
 
 int main(int argc, char **argv)
 {
@@ -31,10 +48,14 @@ void lsh_loop()
 
     do
     {
-        printf(">");
+        printf("> ");
+        fflush(stdout);
         line = lsh_readline();
-        args = lsh_split_line();
-        status = lsh_execute_line();
+        args = lsh_split_line(line);
+        status = lsh_execute_line(args);
+
+        free(line);
+        free(args);
     } while (status);
 }
 
@@ -63,7 +84,7 @@ char *lsh_readline()
         }
         position++;
 
-        if (position >= buffer)
+        if (position >= capacity)
         {
             capacity *= 2;
             char *tmp = realloc(buffer, capacity);
@@ -96,10 +117,10 @@ char **lsh_split_line(char *line)
         buffer[position] = token;
         position++;
 
-        if (position >= buffer)
+        if (position >= capacity)
         {
             capacity *= 2;
-            char *tmp = realloc(buffer, capacity * sizeof(char *));
+            char **tmp = realloc(buffer, capacity * sizeof(char *));
             if (!tmp)
             {
                 fprintf(stderr, "lsh: memory reallocation error\n");
@@ -113,6 +134,92 @@ char **lsh_split_line(char *line)
     return buffer;
 }
 
-int lsh_execute_line()
+int lsh_launch(char **args)
 {
+    pid_t pid;
+    pid_t wpid;
+    int status = 0;
+
+    pid = fork();
+    if (pid == 0)
+    // Child Process
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        perror("lsh");
+    }
+    else
+    {
+        // Parent proces
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+int lsh_execute_line(char **args)
+{
+    int i;
+    if (args[0] == NULL)
+    {
+        return 1;
+    }
+    for (int i = 0; i < lsh_num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return lsh_launch(args);
+}
+
+int lsh_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Krushna Shell\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
 }
